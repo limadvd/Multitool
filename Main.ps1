6171692dab98ed0fe7cb5ef9600fe81339ea2127
limadvd <david.coelho@shopee.com>
2026-02-09 11:34:29 -0300

    Modify admin check and import necessary modules
    Updated admin check message and added module imports.

--- Main.ps1
+++ Main.ps1
@@ -1,13 +1,19 @@
+# Execução como admin
 if (-not ([Security.Principal.WindowsPrincipal]
     [Security.Principal.WindowsIdentity]::GetCurrent()
 ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Host "❌ Execute como Administrador" -ForegroundColor Red
-    Pause
+    Write-Host "Execute como administrador"
     exit
 }
 
-$BaseDir = "C:\Teste Rede"
-New-Item -ItemType Directory -Path $BaseDir -Force | Out-Null
-$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$RunDir = "$BaseDir\Exec_$timestamp"
-New-Item -ItemType Directory -Path $RunDir -Force | Out-Null
+# Puxar config
+. "$PSScriptRoot\config.ps1"
+
+# Importar módulos
+Import-Module "$PSScriptRoot\modules\Rede.psm1" -Force
+Import-Module "$PSScriptRoot\modules\Bateria.psm1" -Force
+Import-Module "$PSScriptRoot\modules\Sistema.psm1" -Force
+Import-Module "$PSScriptRoot\modules\Multimidia.psm1" -Force
+Import-Module "$PSScriptRoot\modules\FastStartup.psm1" -Force
+Import-Module "$PSScriptRoot\modules\Lenovo.psm1" -Force
+Import-Module "$PSScriptRoot\modules\Decrapifier.psm1" -Force
